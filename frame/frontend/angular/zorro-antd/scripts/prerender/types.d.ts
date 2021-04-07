afef4d7df3aadb5c2c201aeec9bf6a5e7a3700e3
zc-r <36868904+zc-r@users.noreply.github.com>
2021-04-07 22:48:21 +0800

    Delete frame/frontend/angular/zorro-antd directory

--- frame/frontend/angular/zorro-antd/scripts/prerender/types.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'sitemap' {
-  interface Sitemap {
-    toXML(fun: (err: Error, xml: string) => void): void;
-    toString(): string;
-  }
-  // tslint:disable-next-line:no-any
-  export function createSitemap(option: any): Sitemap;
-}
--- frame/frontend/angular/zorro-antd/scripts/release-helper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-rm -rf archive-docs
-git clone https://github.com/NG-ZORRO/archive-docs.git
-cp -r archive-docs/issue-helper dist/issue-helper
-cp -r archive-docs/version dist/version
-rm -rf archive-docs
