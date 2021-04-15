061614511d9991d20d9c213d368b53c305039e61
zc-r <xiaoxiaoxfu@qq.com>
2021-04-15 16:33:39 +0800

    [modify] 左侧菜单样式 === 结束

--- frame/frontend/angular/easy-unit-angular/scripts/site/_site/doc/app/shared/side/side.component.ts
+++ frame/frontend/angular/easy-unit-angular/scripts/site/_site/doc/app/shared/side/side.component.ts
@@ -1,16 +1,24 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'demo-eu-side',
   templateUrl: './side.component.html',
   styleUrls: ['./side.component.scss']
 })
-export class SideComponent {
+export class SideComponent implements OnInit, AfterViewInit {
 
-  restrictOneOpen = false;
-  accordionTypeEmbed = false;
+  ngOnInit(): void {
+    console.log('.....................')
+  }
+
+  ngAfterViewInit(): void {
+    const firstChild = document.getElementById('menu')?.children[0];
+    firstChild?.setAttribute('style', 'box-shadow: none');
+  }
+
   sideMenuList = [{
     title: 'Content 1',
+    open: true,
     children: [
       { title: 'Child Content 1' },
       { title: 'Child Content 2' },
@@ -18,10 +26,17 @@ export class SideComponent {
     ]
   }, {
     title: 'Content 2（This is a long sentence for option display.）',
+    open: true,
     children: [
       { title: 'Child Content 1 (This is a long sentence for option display.)' },
       { title: 'Child Content 2' },
-      { title: 'Child Content 3' },
+      {
+        title: 'Child Content 3', children: [
+          { title: 'Child Content 1 (This is a long sentence for option display.)' },
+          { title: 'Child Content 2' },
+          { title: 'Child Content 3' },
+        ]
+      },
     ]
   }, {
     title: 'Content 3 (Default Open)',
@@ -33,39 +48,20 @@ export class SideComponent {
     ]
   }, {
     title: 'Content 4 (No Child)',
+    open: true,
     children: []
   }, {
     title: 'Content 5 (Disabled)',
+    open: true,
     disabled: true,
     children: [
     ]
   }, {
     title: 'Content 6 (Dynamic Content)',
+    open: true,
     needLoadChildren: true,
     loading: false,
     children: [
     ]
   }];
-
-  itemClick(event: any) {
-    console.log('item click' + JSON.stringify(event));
-  }
-  menuToggle(event: any) {
-    console.log('menu toggle' + JSON.stringify(event));
-
-    if (event.open && event.item.needLoadChildren) {
-      event.item.loading = true;
-      setTimeout(() => {
-        event.item.children = [
-          { title: 'Child Content 1' },
-          { title: 'Child Content 2' }
-        ];
-        event.item.needLoadChildren = false;
-        event.item.loading = false;
-      }, 1000);
-    }
-  }
-  log(...v: any) {
-    console.log(...v);
-  }
 }
