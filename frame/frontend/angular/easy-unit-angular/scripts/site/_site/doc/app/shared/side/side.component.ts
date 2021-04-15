987a8281f3ced41b6ae6e639364d0aafeb3db7a1
zc-r <xiaoxiaoxfu@qq.com>
2021-04-15 17:00:49 +0800

    [modify] 微调整报错

--- frame/frontend/angular/easy-unit-angular/scripts/site/_site/doc/app/shared/side/side.component.ts
+++ frame/frontend/angular/easy-unit-angular/scripts/site/_site/doc/app/shared/side/side.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'demo-eu-side',
@@ -7,61 +8,18 @@ import { AfterViewInit, Component, OnInit } from "@angular/core";
 })
 export class SideComponent implements OnInit, AfterViewInit {
 
+  sideMenuList: any = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
   ngOnInit(): void {
-    console.log('.....................')
+    this.http.get('./assets/menu.json').toPromise().then((res) => this.sideMenuList = res);
   }
 
   ngAfterViewInit(): void {
     const firstChild = document.getElementById('menu')?.children[0];
     firstChild?.setAttribute('style', 'box-shadow: none');
   }
-
-  sideMenuList = [{
-    title: 'Content 1',
-    open: true,
-    children: [
-      { title: 'Child Content 1' },
-      { title: 'Child Content 2' },
-      { title: 'Child Content ' },
-    ]
-  }, {
-    title: 'Content 2（This is a long sentence for option display.）',
-    open: true,
-    children: [
-      { title: 'Child Content 1 (This is a long sentence for option display.)' },
-      { title: 'Child Content 2' },
-      {
-        title: 'Child Content 3', children: [
-          { title: 'Child Content 1 (This is a long sentence for option display.)' },
-          { title: 'Child Content 2' },
-          { title: 'Child Content 3' },
-        ]
-      },
-    ]
-  }, {
-    title: 'Content 3 (Default Open)',
-    open: true,
-    children: [
-      { title: 'Child Content 1 (Disabled)', disabled: true },
-      { title: 'Child Content 2 (Default Active)', active: true },
-      { title: 'Child Content 3' },
-    ]
-  }, {
-    title: 'Content 4 (No Child)',
-    open: true,
-    children: []
-  }, {
-    title: 'Content 5 (Disabled)',
-    open: true,
-    disabled: true,
-    children: [
-    ]
-  }, {
-    title: 'Content 6 (Dynamic Content)',
-    open: true,
-    needLoadChildren: true,
-    loading: false,
-    children: [
-    ]
-  }];
 }
