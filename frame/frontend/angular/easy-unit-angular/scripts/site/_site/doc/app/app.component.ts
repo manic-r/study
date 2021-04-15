3fc5fbf30d885ff5ad4882874abac481a81e5a67
zc-r <xiaoxiaoxfu@qq.com>
2021-04-15 18:56:42 +0800

    [modify] 修改页面布局方式

--- frame/frontend/angular/easy-unit-angular/scripts/site/_site/doc/app/app.component.ts
+++ frame/frontend/angular/easy-unit-angular/scripts/site/_site/doc/app/app.component.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'easy-unit-angular';
   lang: string;
 
   constructor(
-    private translate: TranslateService
+    private translate: TranslateService,
+    private http: HttpClient
   ) {
     this.lang = this.translate.getBrowserLang();
   }
 
   selectChange() {
     this.translate.use(this.lang);
+  }
+
+  //////////////////////////////
+  sideMenuList: any = [];
+
+
+  ngOnInit(): void {
+    this.http.get('./assets/menu.json').toPromise().then((res) => this.sideMenuList = res);
+  }
+
+  ngAfterViewInit(): void {
+    const menu: HTMLElement = <HTMLElement>document.getElementById('menu') || {};
+    // 设置插件宽度，防止滚动条出现占用位置
+    // menu?.parentElement?.clientWidth
+    // menu.parentElement
+    // 去掉插件外边框
+    // (menu.firstChild as HTMLElement)?.setAttribute('style', 'box-shadow: none');
   }
 }
