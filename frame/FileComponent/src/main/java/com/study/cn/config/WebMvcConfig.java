b71e68a2f5551d53d8a38be72b8db8be6f374ff8
zc-r <xiaoxiaoxfu@qq.com>
2021-05-12 23:57:28 +0800

    [modify] 删除多余文件

--- frame/FileComponent/src/main/java/com/study/cn/config/WebMvcConfig.java
+++ frame/FileComponent/src/main/java/com/study/cn/config/WebMvcConfig.java
@@ -1,16 +1,17 @@
 package com.study.cn.config;
 
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebMvcConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurerAdapter corsConfigurer() {
+//        return new WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter();
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
