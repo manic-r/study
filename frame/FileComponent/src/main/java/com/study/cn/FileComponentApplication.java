1db74383c70544dba35863108a7e577f8ca05a30
zc-r <xiaoxiaoxfu@qq.com>
2021-05-14 17:45:38 +0800

    [delete] 删除webapp文件

--- frame/FileComponent/src/main/java/com/study/cn/FileComponentApplication.java
+++ frame/FileComponent/src/main/java/com/study/cn/FileComponentApplication.java
@@ -2,13 +2,24 @@ package com.study.cn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
 // https://blog.csdn.net/weixin_39807541/article/details/111360952
 @SpringBootApplication
 public class FileComponentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FileComponentApplication.class, args);
+	public static void main(String[] args) throws UnknownHostException {
+		ConfigurableApplicationContext context = SpringApplication.run(FileComponentApplication.class, args);
+		Environment env = context.getBean(Environment.class);
+		String port = env.getProperty("server.port") == null ? "8080" : env.getProperty("server.port");
+
+		System.out.println("Server running at:");
+		System.out.println("- Local:   " +  "http://localhost:" + port);
+		System.out.println("- Network: " +  "http://" + Inet4Address.getLocalHost().getHostAddress() + ":" + port);
 	}
 
 }
